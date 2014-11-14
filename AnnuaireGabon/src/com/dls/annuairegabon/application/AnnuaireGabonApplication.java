c00714195d31049e2dc86efefaa11dc5d3e49a0f
dima.dze <dima.dze@mail.com>
2014-11-14 15:19:45 +0000

    Minor

--- AnnuaireGabon/src/com/dls/annuairegabon/application/AnnuaireGabonApplication.java
+++ AnnuaireGabon/src/com/dls/annuairegabon/application/AnnuaireGabonApplication.java
@@ -1,7 +1,6 @@
 package com.dls.annuairegabon.application;
 
 import android.app.Application;
-import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 
@@ -19,7 +18,7 @@ public class AnnuaireGabonApplication extends Application {
 	public final String EMPTY_STRING = "";
 	
 	/** Current context & activity */
-	private static Context context;
+//	private static Context context;
 	
 	/** Current Screen Type & Fragment */
 	private AnnuaireGabonScreenType currentScreenType;
@@ -34,7 +33,7 @@ public class AnnuaireGabonApplication extends Application {
 		Log.d(LOG_TAG, "On Create");
 		
 		// Instantiate the context
-		context = getApplicationContext();
+//		context = getApplicationContext();
 	}
 
 	// ----------------------------------------- Getters & Setters -----------------------------------------//
